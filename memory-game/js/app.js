b4fc3f66b3c8fca589ca80287e8c86122454b226
xumeishan <992910205@qq.com>
2017-12-05 18:51:57 +0800

    完成记忆游戏项目

--- memory-game/js/app.js
+++ memory-game/js/app.js
@@ -1,14 +1,96 @@
+
+var getUL=document.getElementById('deck'),
+    list=document.getElementsByClassName('card'),
+    restart=document.getElementById('restart'),
+    clickList_two=[],indexContainer=[];
+
 /*
- * Create a list that holds all of your cards
- */
+* Create a list that holds all of your cards
+*/
+function getLiList() {
+    var tmpList=[];
+    for(var i=0;i<list.length;i++){
+        var liList_item={};
+        liList_item=list[i];
+        tmpList.push(liList_item);
+    }
+    var randomList=shuffle(tmpList);
+    render__liList(randomList);
+}
+
+/*
+* 渲染每个list
+*/
+
+function render__liList(array) {
+    getUL.innerHTML='';
+    for(var i=0;i<array.length;i++){
+        getUL.appendChild(array[i]);
+    }
+    listening__click();
+}
+
+/*
+*   重置按钮
+*/
+restart.addEventListener('click',function () {
+    for(var i=0;i<list.length;i++){
+        list[i].style.cssText ='transform:rotateY(0)';
+    }
+    getLiList();
+});
 
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+*监听点击事件
+*/
+function listening__click() {
+    for(var i=0;i<list.length;i++){
+        list[i].count=i;
+        list[i].onclick=function () {
+            judgeResult(this.count);
+        };
+    }
+}
+
+
+/*
+*判断结果函数
+*/
+function judgeResult(index) {
+    indexContainer.push(index);
+    console.log(indexContainer);
+    var clickList={};
+    clickList.i_className=list[index].lastElementChild.firstElementChild.getAttribute('class');
+    clickList_two.push(clickList);
+
+    if(clickList_two.length===2){
+        if (clickList_two[0].i_className===clickList_two[1].i_className){
+            for (var j=0 ;j<indexContainer.length;j++){
+
+                list[indexContainer[j]].style.cssText =
+                    'animation:changeScale 0.6s ease';
+                list[indexContainer[j]].lastElementChild.style.background='#02ccba';
+                list[indexContainer[j]].addEventListener('webkitAnimationEnd',function () {
+                    this.style.webkitAnimation='';
+                });
+            }
+        }
+        else {
+            for (var j=0 ;j<indexContainer.length;j++){
+                list[indexContainer[j]].style.cssText =
+                        'animation:changeSkew 0.6s ease;transform-origin:50% 150%';
+                list[indexContainer[j]].lastElementChild.style.background='#de3f40';
+                list[indexContainer[j]].addEventListener('webkitAnimationEnd',function () {
+                    this.style.cssText ='animation:changeRotate 0.6s ease reverse forwards;';
+                });
+            }
+        }
+        clickList_two=[];
+        indexContainer=[];
+    }
+}
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -21,18 +103,20 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
+function init() {
+    getLiList();
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+    setTimeout(function () {
+        for(var i=0;i<list.length;i++){
+            list[i].style.cssText =
+                'animation:changeRotate 0.6s ease reverse forwards;' +
+                'transform:rotateY(0)';
+        }
+    },1000);
+    listening__click();
+}
+
+init();
